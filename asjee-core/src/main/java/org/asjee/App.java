a99afc20d0f168e1f6f9a78366cfc82fc146547c
chenas <chenaschen@gmail.com>
2016-02-25 17:56:21 +0800

    maven configuration

---
+++ asjee-core/src/main/java/org/asjee/App.java
@@ -0,0 +1,20 @@
+package org.asjee;
+
+import org.asjee.Th;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	 *@javadoc
+	 *this is test
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Th t = new Th();
+        t.sayHello();
+    }
+}
---
+++ asjee-core/src/main/java/org/asjee/Th.java
@@ -0,0 +1,12 @@
+package org.asjee;
+public class Th
+{
+	/**
+	*
+	* @param test
+	*/
+	public void sayHello()
+	{
+		System.out.println("hello world");
+	}
+}
